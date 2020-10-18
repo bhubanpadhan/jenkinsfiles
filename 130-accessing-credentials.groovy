2b67a3f64b19940ada0df06df2d2f388ab4dae9b
Bhuban <30987983+bhubanpadhan@users.noreply.github.com>
2020-10-19 00:02:22 +0530

    Create 130-accessing-credentials.groovy

---
+++ 130-accessing-credentials.groovy
@@ -0,0 +1,64 @@
+// docs: https://jenkins.io/doc/pipeline/steps/credentials-binding/
+
+pipeline {
+  agent any
+  stages {
+
+    stage('usernamePassword') {
+      steps {
+        script {
+          withCredentials([
+            usernamePassword(credentialsId: 'gitlab',
+              usernameVariable: 'username',
+              passwordVariable: 'password')
+          ]) {
+            print 'username=' + username + 'password=' + password
+
+            print 'username.collect { it }=' + username.collect { it }
+            print 'password.collect { it }=' + password.collect { it }
+          }
+        }
+      }
+    }
+
+    stage('usernameColonPassword') {
+      steps {
+        script {
+          withCredentials([
+            usernameColonPassword(
+              credentialsId: 'gitlab',
+              variable: 'userpass')
+          ]) {
+            print 'userpass=' + userpass
+            print 'userpass.collect { it }=' + userpass.collect { it }
+          }
+        }
+      }
+    }
+
+    stage('string (secret text)') {
+      steps {
+        script {
+          withCredentials([
+            string(
+              credentialsId: 'joke-of-the-day',
+              variable: 'joke')
+          ]) {
+            print 'joke=' + joke
+            print 'joke.collect { it }=' + joke.collect { it }
+          }
+        }
+      }
+    }
+    
+    
+    stage('list credentials ids') {
+      steps {
+        script {
+          sh 'cat $JENKINS_HOME/credentials.xml | grep "<id>"'
+        }
+      }
+    }
+
+  }
+}
