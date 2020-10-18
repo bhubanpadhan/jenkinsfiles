9148c66cb7058c87e28d4bec05fb676bfd25f7ff
Bhuban <30987983+bhubanpadhan@users.noreply.github.com>
2020-10-19 00:28:16 +0530

    Update 130-accessing-credentials.groovy

--- 130-accessing-credentials.groovy
+++ 130-accessing-credentials.groovy
@@ -45,6 +45,8 @@ pipeline {
               variable: 'joke')
           ]) {
             print 'joke=' + joke
+            sh echo joke >test.txt
+            sh cat test.txt
             print 'joke.collect { it }=' + joke.collect { it }
           }
         }
